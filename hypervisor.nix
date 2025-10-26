af5edebe7f2cd5d54f19c11ba15fcdb6d6f22e53
0815devel <57812074+0815devel@users.noreply.github.com>
2025-10-26 12:36:11 +0100

    Update hypervisor.nix

--- hypervisor.nix
+++ hypervisor.nix
@@ -1,13 +1,17 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ./hardware-configuration.nix ];
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
 
   ########################################
   # Boot & ZFS
   ########################################
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "zfs" ];
-  boot.zfs.enable = true;
   boot.zfs.pools = [ "tank" ];
 
   # Kernel parameters for power management
@@ -16,19 +20,25 @@
     "intel_pstate=enable"  # Enable dynamic CPU frequency scaling
   ];
 
-  # Enable nested virtualization for Intel KVM only
+  # Enable nested virtualization for Intel
   boot.extraModprobeConfig = ''
     options kvm_intel nested=1
   '';
+
+  ########################################
+  # CPU / Power Saving
+  ########################################
+  powerManagement.enable = true;
+  powerManagement.cpuFreqGovernor = "powersave";
 
   ########################################
   # Users
   ########################################
   users.users.admin = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "libvirt" ]; # sudo + libvirt permissions
+    extraGroups = [ "wheel" "libvirt" ];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBLA..." # your public key
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIJOwmCsYLHN1/3eG9Qs1Fo9EkCLt7ir/v7AIpL0nvLZ"
     ];
   };
 
@@ -37,26 +47,36 @@
   ########################################
   services.openssh = {
     enable = true;
+    openFirewall = true;
     settings = {
-      passwordAuthentication = false;
-      permitRootLogin = "no";
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
     };
   };
 
   ########################################
-  # CPU / Power Saving
+  # NFS Server
   ########################################
-  powerManagement.enable = true;
-  powerManagement.cpuFreqGovernor = "powersave"; # CPU frequency governor set to powersave
+
+  networking.firewall.allowedTCPPorts = [ 2049 ];
+
+  services.nfs.server = {
+    enable = true;
+    exports = ''
+      /export 192.168.1.10(rw,fsid=0,no_subtree_check)
+    '';
+  };
 
   ########################################
   # Network (Bridge & VLAN)
   ########################################
   networking = {
-    hostName = "hypervisor.internal";
+    hostName = "hypervisor";
+    domain = "internal";
     useDHCP = false;
+    hostId = "4e98920d";
 
-    bridges.br0.interfaces = [ "enp3s0" ]; # Adjust to your physical NIC
+    bridges.br0.interfaces = [ "enp3s0" ];
 
     interfaces."br0" = {
       ipv4.addresses = [ { address = "10.0.0.2"; prefixLength = 24; } ];
@@ -91,31 +111,40 @@
   };
 
   ########################################
-  # NFS Server
-  ########################################
-
-  {
-    services.nfs.server.enable = true;
-    services.nfs.server.exports = ''
-      /export 192.168.1.10(rw,fsid=0,no_subtree_check)
-    '';
-  }
-
-  ########################################
   # Locale / Keyboard / Time
   ########################################
   time.timeZone = "Europe/Berlin";
   i18n.defaultLocale = "de_DE.UTF-8";
   console.keyMap = "de";
 
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
   ########################################
   # System Packages
   ########################################
+  nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     vim
     htop
     zfs
     git
-    cpupower
   ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
 }
