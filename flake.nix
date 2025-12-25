b5cd5d580d1e4716a196cc6f0e30790df87ec25c
System administrator <root@hypervisor.internal>
2025-12-25 23:51:37 +0100

    update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, ... }:
     nixosConfigurations.hypervisor = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
