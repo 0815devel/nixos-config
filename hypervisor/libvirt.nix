75151a355f27bdc448f2711d0046b2e36e1ede8d
System administrator <root@hypervisor.internal>
2025-12-26 15:30:53 +0100

    optimized

--- hypervisor/libvirt.nix
+++ hypervisor/libvirt.nix
@@ -12,4 +12,21 @@
       swtpm.enable = true;
     };
   };
+
+  ########################################
+  # IOMMU & Nested
+  ########################################
+  boot = {
+    #kernelParams = [
+      #"intel_iommu=on" # Enable IOMMU
+      #"iommu=pt" # Performance for IOMMU
+      #"vfio-pci.ids=1002:67b0,1002:aac8" # IDs of PCIe devices to passthrough
+    #];
+
+    #kernelModules = [ "vfio" "vfio_pci" "vfio_iommu_type1" ]; # VFIO modules for PCIe Passthrough
+
+    extraModprobeConfig = ''
+      options kvm_intel nested=1 # Neasted virtualization
+    '';
+  };
 }
