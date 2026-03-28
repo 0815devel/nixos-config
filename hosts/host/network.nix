24e804c7cad5ddd67f59b107af617a5a6bdb7945
0815devel <booslukas@gmail.com>
2026-03-28 18:33:43 +0100

    deleted br-wan

--- hosts/host/network.nix
+++ hosts/host/network.nix
@@ -19,25 +19,12 @@
           Name = "br-lan";
          };
        };
-      "br-wan" = {
-        netdevConfig = {
-          Kind = "bridge";
-          Name = "br-wan";
-         };
-       };
       "br-nfs" = {
         netdevConfig = {
           Kind = "bridge";
           Name = "br-nfs";
          };
        };
-      "lan-nic0-vlan7" = {
-        netdevConfig = {
-          Kind = "vlan";
-          Name = "lan-nic0.7";
-        };
-        vlanConfig.Id = 7;
-      };
     };
     networks = {
       "lan-onboard" = {
@@ -66,24 +53,12 @@
         networkConfig.Bridge = "br-lan";
         linkConfig.RequiredForOnline = "enslaved";
       };
-      "lan-nic0.7" = {
-        matchConfig.Name = "lan-nic0.7";
-        networkConfig.Bridge = "br-wan";
-        linkConfig.RequiredForOnline = "carrier";
-      };
       "br-lan" = {
         matchConfig.Name = "br-lan";
         address = [ "10.0.0.3/24" ];
         dns = [ "10.0.0.1" "1.1.1.1" ];
         domains = [ "internal" ];
         gateway = [ "10.0.0.1" ];
-      };
-      "br-wan" = {
-        matchConfig.Name = "br-wan";
-        linkConfig.ActivationPolicy = "always-up";
-        networkConfig = {
-          ConfigureWithoutCarrier = true;
-        };
       };
       "br-nfs" = {
         matchConfig.Name = "br-nfs";
