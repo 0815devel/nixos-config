1cc77d8c611641d5c9b41919d16112709477ff8f
0815devel <booslukas@gmail.com>
2026-04-25 22:08:08 +0200

    enable nix-ld

--- machines/host/default.nix
+++ machines/host/default.nix
@@ -56,6 +56,8 @@
     nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
   };
 
+  programs.nix-ld.enable = true;
+
   system.configurationRevision = inputs.self.rev or "dirty";
 
   # This value determines the NixOS release from which the default
