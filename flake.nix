7809a7a6124f98246ccda8a0f53dba4b99f95923
0815devel <57812074+0815devel@users.noreply.github.com>
2026-06-12 12:41:12 +0200

    Add sops-nix input to flake.nix

--- flake.nix
+++ flake.nix
@@ -5,12 +5,17 @@
       url = "github:microvm-nix/microvm.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
     {
       self,
       nixpkgs,
       microvm,
+      sops-nix,
       ...
     }@inputs:
     let
@@ -19,7 +24,7 @@
     {
       nixosConfigurations."host" = nixpkgs.lib.nixosSystem {
         inherit system;
-        specialArgs = { inherit inputs microvm; };
+        specialArgs = { inherit inputs microvm sops-nix; };
         modules = [ ./machines/host ];
       };
     };
