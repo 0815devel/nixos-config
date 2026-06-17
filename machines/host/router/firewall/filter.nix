8a2e8d8681b2cf05fee95780d5694a87092c4166
0815devel <57812074+0815devel@users.noreply.github.com>
2026-06-17 08:15:14 +0200

    Update filter.nix

--- machines/host/router/firewall/filter.nix
+++ machines/host/router/firewall/filter.nix
@@ -1,54 +1,51 @@
 { ... }:
 
 {
-  networking.nftables.tables = {
+  networking.nftables.tables."filter" = {
+    family = "inet";
+    content = ''
+      flush ruleset
 
-    filter = {
-      family = "inet";
-      content = ''
-        flush ruleset
+      define LAN = "lan"
+      define WAN = "pppoe0"
+      define MODEM = "modem"
+      define WIREGUARD = "wg0"
 
-        define LAN = "lan"
-        define WAN = "pppoe0"
-        define MODEM = "modem"
-        define WIREGUARD = "wg0"
+      table inet filter {
 
-        table inet filter {
-
-          chain output {
-            type filter hook output priority 0;
-            policy accept;
-          }
-
-          chain input {
-            type filter hook input priority 0;
-            policy drop;
-
-            iifname lo accept;
-            ct state established,related accept;
-
-            icmp type echo-request accept;
-            icmp type { destination-unreachable, time-exceeded, parameter-problem } accept;
-
-            iifname $LAN accept;
-
-            iifname $WAN udp dport 51820 accept;
-          }
-
-          chain forward {
-            type filter hook forward priority 0;
-            policy drop;
-
-            tcp flags syn tcp option maxseg size set rt mtu
-
-            iifname $LAN oifname $WAN accept;
-            iifname $WAN oifname $LAN ct state established,related accept;
-            iifname $LAN oifname $LAN accept;
-
-            iifname $WAN ip daddr 10.0.0.2 tcp dport { 80, 443 } accept;
-          }
+        chain output {
+          type filter hook output priority 0;
+          policy accept;
         }
-      '';
-    };
+
+        chain input {
+          type filter hook input priority 0;
+          policy drop;
+
+          iifname lo accept;
+          ct state established,related accept;
+
+          icmp type echo-request accept;
+          icmp type { destination-unreachable, time-exceeded, parameter-problem } accept;
+
+          iifname $LAN accept;
+
+          iifname $WAN udp dport 51820 accept;
+        }
+
+        chain forward {
+          type filter hook forward priority 0;
+          policy drop;
+
+          tcp flags syn tcp option maxseg size set rt mtu
+
+          iifname $LAN oifname $WAN accept;
+          iifname $WAN oifname $LAN ct state established,related accept;
+          iifname $LAN oifname $LAN accept;
+
+          iifname $WAN ip daddr 10.0.0.2 tcp dport { 80, 443 } accept;
+        }
+      }
+    '';
   };
 }
