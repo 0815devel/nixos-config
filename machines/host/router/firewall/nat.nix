a5cc0578c46c5ffaee97b624846afb9a068944ac
0815devel <57812074+0815devel@users.noreply.github.com>
2026-06-17 08:12:54 +0200

    Create nat.nix

---
+++ machines/host/router/firewall/nat.nix
@@ -0,0 +1,33 @@
+{ ... }:
+
+{
+  networking.nftables.tables."nat" = {
+    family = "ip";
+    content = ''
+      define LAN = "lan"
+      define WAN = "pppoe0"
+      define MODEM = "modem"
+      define WIREGUARD = "wg0"
+
+      table ip nat {
+
+        chain prerouting {
+          type nat hook prerouting priority 0;
+
+          iifname $WAN tcp dport 80 dnat to 10.0.0.2:80;
+          iifname $WAN tcp dport 443 dnat to 10.0.0.2:443;
+
+          iifname $LAN udp dport 53 ip daddr != 10.0.0.1 dnat to 10.0.0.1;
+        }
+
+        chain postrouting {
+          type nat hook postrouting priority 100;
+
+          iifname $LAN oifname $WAN masquerade;
+          iifname $LAN oifname $MODEM masquerade;
+          iifname $WIREGUARD oifname $WAN masquerade;
+        }
+      }
+    '';
+  };
+}
