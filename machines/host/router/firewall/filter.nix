cdc86ee6db2b06f8ca71ada506c45e93b557f104
0815devel <booslukas@gmail.com>
2026-07-19 14:33:32 +0200

    updated firewall

--- machines/host/router/firewall/filter.nix
+++ machines/host/router/firewall/filter.nix
@@ -9,65 +9,56 @@
       define MODEM = "modem"
       define HOME = "wg0"
       define NETHERLANDS = "wg1"
+      define JELLYFIN = "vm-nfs-jellyfin"
+      define NAVIDROME = "vm-nfs-music"
+      define IMMICH = "vm-nfs-immich"
 
-      table ip filter {
-
-        chain output {
-          type filter hook output priority 0;
-          policy accept;
-        }
-
-        chain input {
-          type filter hook input priority 0;
-          policy drop;
-
-          iifname lo accept;
-          ct state established,related accept;
-
-          icmp type echo-request accept;
-          icmp type { destination-unreachable, time-exceeded, parameter-problem } accept;
-
-          iifname $LAN accept;
-
-          iifname $WAN udp dport 51820 accept;
-        }
-
-        chain forward {
-          type filter hook forward priority 0;
-          policy drop;
-
-          tcp flags syn tcp option maxseg size set rt mtu
-
-          iifname $LAN oifname $WAN accept;
-          iifname $WAN oifname $LAN ct state established,related accept;
-          iifname $LAN oifname $LAN accept;
-
-          iifname $WAN ip daddr 10.0.0.2 tcp dport { 80, 443 } accept;
-
-          iifname $HOME oifname $WAN accept;
-          iifname $NETHERLANDS oifname $WAN accept;
-        
-
-      iifname "vm-nfs-jellyfin" ip saddr 10.0.1.20 tcp dport 2049 accept
-      iifname "vm-nfs-jellyfin" ip saddr 10.0.1.20 udp dport 2049 accept
-
-      iifname "vm-nfs-music" ip saddr 10.0.1.22 tcp dport 2049 accept
-      iifname "vm-nfs-music" ip saddr 10.0.1.22 udp dport 2049 accept
-
-      iifname "vm-nfs-immich" ip saddr 10.0.1.24 tcp dport 2049 accept
-      iifname "vm-nfs-immich" ip saddr 10.0.1.24 udp dport 2049 accept
-
-      iifname "vm-nfs-jellyfin" drop
-      iifname "vm-nfs-music" drop
-      iifname "vm-nfs-immich" drop
-
-
-
-
-
-
-}
+      chain output {
+        type filter hook output priority 0;
+        policy accept;
       }
+
+      chain input {
+        type filter hook input priority 0;
+        policy drop;
+
+        iifname lo accept;
+        ct state established,related accept;
+
+        icmp type echo-request accept;
+        icmp type { destination-unreachable, time-exceeded, parameter-problem } accept;
+
+        iifname $LAN accept;
+
+        iifname $WAN udp dport 51820 accept;
+      }
+
+      chain forward {
+        type filter hook forward priority 0;
+        policy drop;
+
+        ct state established,related accept;
+
+        iifname $JELLYFIN ip saddr != 10.0.1.20 drop;
+        iifname $NAVIDROME ip saddr != 10.0.1.22 drop;
+        iifname $IMMICH ip saddr != 10.0.1.24 drop;
+
+        tcp flags syn tcp option maxseg size set rt mtu
+
+        iifname $LAN oifname $WAN accept;
+        iifname $WAN oifname $LAN ct state established,related accept;
+        iifname $LAN oifname $LAN accept;
+
+        iifname $WAN ip daddr 10.0.0.2 tcp dport { 80, 443 } accept;
+
+        iifname $HOME oifname $WAN accept;
+        iifname $NETHERLANDS oifname $WAN accept;
+
+        iifname $JELLYFIN ip saddr 10.0.1.20 ip daddr 10.0.1.2 tcp dport 2049 accept;
+        iifname $NAVIDROME ip saddr 10.0.1.22 ip daddr 10.0.1.2 tcp dport 2049 accept;
+        iifname $IMMICH ip saddr 10.0.1.24 ip daddr 10.0.1.2 tcp dport 2049 accept;
+
+    }
     '';
   };
 }
--- machines/host/router/firewall/nat.nix
+++ machines/host/router/firewall/nat.nix
@@ -10,25 +10,22 @@
       define HOME = "wg0"
       define NETHERLANDS = "wg1"
 
-      table ip nat {
+      chain prerouting {
+        type nat hook prerouting priority 0;
 
-        chain prerouting {
-          type nat hook prerouting priority 0;
+        iifname $WAN tcp dport 80 dnat to 10.0.0.2:80;
+        iifname $WAN tcp dport 443 dnat to 10.0.0.2:443;
 
-          iifname $WAN tcp dport 80 dnat to 10.0.0.2:80;
-          iifname $WAN tcp dport 443 dnat to 10.0.0.2:443;
+        iifname $LAN udp dport 53 ip daddr != 10.0.0.1 dnat to 10.0.0.1;
+      }
 
-          iifname $LAN udp dport 53 ip daddr != 10.0.0.1 dnat to 10.0.0.1;
-        }
+      chain postrouting {
+        type nat hook postrouting priority 100;
 
-        chain postrouting {
-          type nat hook postrouting priority 100;
-
-          iifname $LAN oifname $WAN masquerade;
-          iifname $LAN oifname $MODEM masquerade;
-          iifname $HOME oifname $WAN masquerade;
-          iifname $NETHERLANDS oifname $WAN masquerade;
-        }
+        iifname $LAN oifname $WAN masquerade;
+        iifname $LAN oifname $MODEM masquerade;
+        iifname $HOME oifname $WAN masquerade;
+        iifname $NETHERLANDS oifname $WAN masquerade;
       }
     '';
   };
