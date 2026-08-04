dfddade56659338bac1631079b91ebe4f1319083
0815devel <57812074+0815devel@users.noreply.github.com>
2026-08-04 09:35:18 +0200

    Update NAT configuration for networking rules

--- machines/host/router/firewall/nat.nix
+++ machines/host/router/firewall/nat.nix
@@ -3,29 +3,44 @@
 {
   networking.nftables.tables."nat" = {
     family = "ip";
+
     content = ''
+      define EDGE = "br-edge"
+
       define LAN = "br-lan"
-      define WAN = "pppoe0"
-      define MODEM = "modem"
+      define GUEST = "br-guest"
+      define IOT = "br-iot"
+
       define HOME = "wg-home"
-      define NETHERLANDS = "wg-ndl"
+      define NETHERLANDS = "wg-nld"
+
 
       chain prerouting {
         type nat hook prerouting priority 0;
 
-        iifname $WAN tcp dport 80 dnat to 10.0.0.2:80;
-        iifname $WAN tcp dport 443 dnat to 10.0.0.2:443;
+        iifname $EDGE tcp dport 80  dnat to 10.10.50.2:80;
+        iifname $EDGE tcp dport 443 dnat to 10.10.50.2:443;
 
-        iifname $LAN udp dport 53 ip daddr != 10.0.0.1 dnat to 10.0.0.1;
+        iifname $LAN udp dport 53 ip daddr != 10.10.10.1 dnat to 10.10.10.1;
+        iifname $LAN tcp dport 53 ip daddr != 10.10.10.1 dnat to 10.10.10.1;
+
+        iifname $GUEST udp dport 53 ip daddr != 10.10.20.1 dnat to 10.10.20.1;
+        iifname $GUEST tcp dport 53 ip daddr != 10.10.20.1 dnat to 10.10.20.1;
+
+        iifname $IOT udp dport 53 ip daddr != 10.10.30.1 dnat to 10.10.30.1;
+        iifname $IOT tcp dport 53 ip daddr != 10.10.30.1 dnat to 10.10.30.1;
       }
+
 
       chain postrouting {
         type nat hook postrouting priority 100;
 
-        iifname $LAN ip saddr 10.0.0.0/24 oifname $WAN masquerade;
-        iifname $LAN ip saddr 10.0.0.0/24 oifname $MODEM masquerade;
-        iifname $HOME ip saddr 10.0.10.0/24 oifname $WAN masquerade;
-        iifname $NETHERLANDS oifname $WAN masquerade;
+        iifname $LAN ip saddr 10.10.10.0/24 oifname $EDGE masquerade;
+        iifname $GUEST ip saddr 10.10.20.0/24 oifname $EDGE masquerade;
+
+        iifname $IOT ip saddr 10.10.30.0/24 oifname $NETHERLANDS masquerade;
+
+        iifname $HOME ip saddr 10.10.0.0/24 oifname $EDGE masquerade;
       }
     '';
   };
