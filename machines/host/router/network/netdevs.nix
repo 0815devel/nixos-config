2f12fe2b3744fc71cc7f781a79f6ea70d81fdbbd
0815devel <57812074+0815devel@users.noreply.github.com>
2026-08-04 08:47:06 +0200

    Add network device configurations for bridges and VLANs

---
+++ machines/host/router/network/netdevs.nix
@@ -0,0 +1,73 @@
+{ ... }:
+
+{
+  systemd.network.netdevs = {
+
+    "br-guest" = {
+      netdevConfig = {
+        Kind = "bridge";
+        Name = "br-guest";
+      };
+    };
+
+    "br-iot" = {
+      netdevConfig = {
+        Kind = "bridge";
+        Name = "br-iot";
+      };
+    };
+
+    "br-nfs" = {
+      netdevConfig = {
+        Kind = "bridge";
+        Name = "br-nfs";
+      };
+    };
+
+    "br-dmz" = {
+      netdevConfig = {
+        Kind = "bridge";
+        Name = "br-dmz";
+      };
+    };
+
+    "br-services" = {
+      netdevConfig = {
+        Kind = "bridge";
+        Name = "br-services";
+      };
+    };
+
+    "br-backup" = {
+      netdevConfig = {
+        Kind = "bridge";
+        Name = "br-backup";
+      };
+    };
+
+
+    "vlan-edge" = {
+      netdevConfig = {
+        Kind = "vlan";
+        Name = "vlan-edge";
+      };
+      vlanConfig.Id = 7;
+    };
+
+    "vlan-guest" = {
+      netdevConfig = {
+        Kind = "vlan";
+        Name = "vlan-guest";
+      };
+      vlanConfig.Id = 20;
+    };
+
+    "vlan-iot" = {
+      netdevConfig = {
+        Kind = "vlan";
+        Name = "vlan-iot";
+      };
+      vlanConfig.Id = 30;
+    };
+  };
+}
