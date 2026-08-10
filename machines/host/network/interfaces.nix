ffd48fb8f639e6c4a46a5ab65e81cf1c2a7e0736
0815devel <57812074+0815devel@users.noreply.github.com>
2026-08-10 11:11:09 +0200

    Rename network interfaces in interfaces.nix

--- machines/host/network/interfaces.nix
+++ machines/host/network/interfaces.nix
@@ -3,35 +3,35 @@
 {
   systemd.network.networks = {
 
-    "lan-onboard" = {
+    "onboard" = {
       matchConfig.MACAddress = "9c:6b:00:39:c9:ce";
       networkConfig.Bridge = "br-lan";
       linkConfig.RequiredForOnline = "enslaved";
     };
 
 
-    "lan-nic0" = {
+    "nic0" = {
       matchConfig.MACAddress = "a0:36:9f:83:e8:10";
       networkConfig.Bridge = "br-lan";
       linkConfig.RequiredForOnline = "enslaved";
     };
 
 
-    "lan-nic1" = {
+    "nic1" = {
       matchConfig.MACAddress = "a0:36:9f:83:e8:11";
       networkConfig.Bridge = "br-lan";
       linkConfig.RequiredForOnline = "enslaved";
     };
 
 
-    "lan-nic2" = {
+    "nic2" = {
       matchConfig.MACAddress = "a0:36:9f:83:e8:12";
       networkConfig.Bridge = "br-lan";
       linkConfig.RequiredForOnline = "enslaved";
     };
 
 
-    "lan-nic3" = {
+    "nic3" = {
       matchConfig.MACAddress = "a0:36:9f:83:e8:13";
       networkConfig.Bridge = "br-lan";
       linkConfig.RequiredForOnline = "enslaved";
