32635ceb7de2498bda593a7e9e7f0eba40735438
0815devel <57812074+0815devel@users.noreply.github.com>
2026-08-10 10:58:22 +0200

    Update vlan.nix

--- machines/host/network/vlan.nix
+++ machines/host/network/vlan.nix
@@ -1,1 +1,59 @@
+{ ... }:
 
+{
+  systemd.network.netdevs = {
+    "vlan-edge" = {
+      netdevConfig = {
+        Kind = "vlan";
+        Name = "vlan-edge";
+      };
+      vlanConfig.Id = 7;
+    };
+
+    "vlan-guest" = {
+      netdevConfig = {
+        Kind = "vlan";
+        Name = "vlan-guest";
+      };
+      vlanConfig.Id = 20;
+    };
+
+    "vlan-iot" = {
+      netdevConfig = {
+        Kind = "vlan";
+        Name = "vlan-iot";
+      };
+      vlanConfig.Id = 30;
+    };
+  };
+
+  systemd.network.networks = {
+    "lan-vlans" = {
+      matchConfig.Name = "br-lan";
+
+      networkConfig.VLAN = [
+        "vlan-edge"
+        "vlan-guest"
+        "vlan-iot"
+      ];
+    };
+
+    "edge-vlan" = {
+      matchConfig.Name = "vlan-edge";
+      networkConfig.Bridge = "br-edge";
+      linkConfig.RequiredForOnline = "enslaved";
+    };
+
+    "guest-vlan" = {
+      matchConfig.Name = "vlan-guest";
+      networkConfig.Bridge = "br-guest";
+      linkConfig.RequiredForOnline = "enslaved";
+    };
+
+    "iot-vlan" = {
+      matchConfig.Name = "vlan-iot";
+      networkConfig.Bridge = "br-iot";
+      linkConfig.RequiredForOnline = "enslaved";
+    };
+  };
+}
