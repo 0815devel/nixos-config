512e4da17854f486bac93bfb75206f82a9d6cb95
0815devel <57812074+0815devel@users.noreply.github.com>
2026-09-04 14:59:32 +0200

    Update reverse proxy hostnames

--- profiles/base/hosts.nix
+++ profiles/base/hosts.nix
@@ -18,10 +18,10 @@
     10.10.10.12 ap2.home.lboos.xyz ap2
 
     # DMZ
-    10.10.50.2 reverse_proxy.internal reverse_proxy
+    10.10.50.2 reverse_proxy.home.lboos.xyz reverse_proxy
 
     # Services
-    10.10.60.10 reverse_proxy.internal reverse_proxy
+    10.10.60.10 reverse_proxy.home.lboos.xyz reverse_proxy
     10.10.60.11 navidrome.home.lboos.xyz navidrome
     10.10.60.12 immich.home.lboos.xyz immich
     10.10.60.13 jellyfin.home.lboos.xyz jellyfin
